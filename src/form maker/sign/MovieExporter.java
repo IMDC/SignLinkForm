07ce6a32ff65a574230fe1b38bd50ba6814b6446
norma <norma@305be5d7-c4fa-43dc-ae32-cc50f3445863>
2008-01-21 16:17:18 +0000

    add changes from sls

--- src/form maker/sign/MovieExporter.java
+++ src/form maker/sign/MovieExporter.java
@@ -5,17 +5,16 @@ package sign;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStream;
 
 /**
  * @author Martin Gerdzhev
- * 
  * @version $Id: $
  */
 public class MovieExporter
 {
-	private String	ffmpeg	= SignUtils.FFMPEGFOLDER.getAbsolutePath() + File.separator;
-	private String [] arguments;
+	private String		ffmpeg	= SignUtils.FFMPEGFOLDER.getAbsolutePath() + File.separator;
+	private String[]	arguments;
 
 	public MovieExporter(String inputFile, String outputFile, String type)
 	{
@@ -23,6 +22,22 @@ public class MovieExporter
 		if (System.getProperty("os.name").toUpperCase().indexOf("WINDOWS") == -1)
 		{ // not windows (It's a mac)
 			ffmpeg += "ffmpeg";
+			try
+			{
+				// make ffmpeg executable
+				Process builder = Runtime.getRuntime().exec("chmod +x " + ffmpeg);
+				builder.waitFor();
+			}
+			catch (IOException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (InterruptedException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}
 		else
 		{
@@ -30,15 +45,17 @@ public class MovieExporter
 		}
 		if (type.equals("flv"))
 		{
-			arguments = new String[] { ffmpeg, "-i", inputFile, "-sameq", "-g", "4", outputFile };
+			// exports with the same quality and makes every 5th frame a keyframe (every 0.33s)
+			arguments = new String[] { ffmpeg, "-i", inputFile, "-sameq", "-g", "5", outputFile };
 		}
 		else if (type.equals("mp4"))
 		{
-			//sets the dimensions to 320x240 and framerate to 15fps and no audio
+			// sets the dimensions to 320x240 and framerate to 15fps and no audio
 			arguments = new String[] { ffmpeg, "-i", inputFile, "-s", "320x240", "-r", "15", "-an", outputFile };
 		}
-		
+
 	}
+
 	public boolean export()
 	{
 		if (export(arguments))
@@ -52,37 +69,65 @@ public class MovieExporter
 			return false;
 		}
 	}
-	
-	private boolean export(String [] args)
+
+	private boolean export(String[] args)
 	{
-		String error = "";
 		try
 		{
-			Process builder = Runtime.getRuntime().exec(args);
-			Scanner err = new Scanner(builder.getErrorStream());
-
-			while (err.hasNextLine())
-			{
-				// System.out.println("printing");
-				error = err.nextLine();
-				System.out.print(error);
-
-			}
-			err.close();
-			builder.waitFor();
+			Process process = Runtime.getRuntime().exec(args);
+			InputHandler errorHandler = new InputHandler(process.getErrorStream(), "Error Stream");
+			errorHandler.start();
+			InputHandler inputHandler = new InputHandler(process.getInputStream(), "Output Stream");
+			inputHandler.start();
+			process.waitFor();
 		}
 		catch (IOException e)
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return false;
-		} catch (InterruptedException e) {
+		}
+		catch (InterruptedException e)
+		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return false;
 		}
-	//	if (error.length() == 0)
-			return true;
-		//return false;
+		return true;
+
+	}
+
+	/**
+	 * Class used for reading the output and error streams of ffmpeg
+	 * 
+	 * @author Martin Gerdzhev
+	 */
+	class InputHandler extends Thread
+	{
+
+		InputStream	input_;
+
+		InputHandler(InputStream input, String name)
+		{
+			super(name);
+			input_ = input;
+		}
+
+		public void run()
+		{
+			try
+			{
+				int c;
+				while ((c = input_.read()) != -1)
+				{
+					System.out.write(c);
+				}
+			}
+			catch (Throwable t)
+			{
+				t.printStackTrace();
+			}
+		}
 
 	}
 }
